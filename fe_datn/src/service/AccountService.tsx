cf330f8e8790f0de8c24c5756b6cbce398ffc54e
LamCao1130 <caohailam3011@gmail.com>
2026-05-22 20:01:54 +0700

    add

--- fe_datn/src/service/AccountService.tsx
+++ fe_datn/src/service/AccountService.tsx
@@ -4,34 +4,31 @@ import { API_URL } from "../axiosConfig/Api";
 
 const AccountService = {
   getAccountRole: async () => {
-    return axiosApi.get(`${API_LOCAL}/api/roles/all`);
+    return axiosApi.get(`${API_URL}/api/roles/all`);
   },
   searchAccounts: async (params: any) => {
-    return axiosApi.post(`${API_LOCAL}/api/accounts/search`, params);
+    return axiosApi.post(`${API_URL}/api/accounts/search`, params);
   },
   createAccount: async (data: any) => {
-    return axiosApi.post(`${API_LOCAL}/api/accounts/create`, data);
+    return axiosApi.post(`${API_URL}/api/accounts/create`, data);
   },
   updateAccount: async (data: any, id: number) => {
-    return axiosApi.put(`${API_LOCAL}/api/accounts/update/${id}`, data);
+    return axiosApi.put(`${API_URL}/api/accounts/update/${id}`, data);
   },
   deleteAccount: async (id: number) => {
-    return axiosApi.delete(`${API_LOCAL}/api/accounts/delete/${id}`);
+    return axiosApi.delete(`${API_URL}/api/accounts/delete/${id}`);
   },
   getAll: async () => {
-    return axiosApi.get(`${API_LOCAL}/api/accounts/all`);
+    return axiosApi.get(`${API_URL}/api/accounts/all`);
   },
   getAccountById: async (id: number) => {
-    return axiosApi.get(`${API_LOCAL}/api/accounts/get-account-id/${id}`);
+    return axiosApi.get(`${API_URL}/api/accounts/get-account-id/${id}`);
   },
   updateProfile: async (data: any, id: number) => {
-    return axiosApi.post(
-      `${API_LOCAL}/api/accounts/change-profile/${id}`,
-      data,
-    );
+    return axiosApi.post(`${API_URL}/api/accounts/change-profile/${id}`, data);
   },
   changePassword: async (data: any) => {
-    return axiosApi.post(`${API_LOCAL}/api/accounts/change-password`, data);
+    return axiosApi.post(`${API_URL}/api/accounts/change-password`, data);
   },
 };
 export default AccountService;
