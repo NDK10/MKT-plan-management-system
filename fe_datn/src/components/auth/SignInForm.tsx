d85f3caf9865eea79d5db5134ac15945c514926a
LamCao1130 <caohailam3011@gmail.com>
2026-05-25 15:10:38 +0700

    add

--- fe_datn/src/components/auth/SignInForm.tsx
+++ fe_datn/src/components/auth/SignInForm.tsx
@@ -117,12 +117,6 @@ export default function SignInForm() {
                       Ghi nhớ
                     </span>
                   </div>
-                  <Link
-                    to="/reset-password"
-                    className="text-sm text-brand-500 hover:text-brand-600 dark:text-brand-400"
-                  >
-                    Quên mật khẩu?
-                  </Link>
                 </div>
                 <div>
                   <Button className="w-full" size="sm" onClick={handleSubmit}>
@@ -131,18 +125,6 @@ export default function SignInForm() {
                 </div>
               </div>
             </form>
-
-            <div className="mt-5">
-              <p className="text-sm font-normal text-center text-gray-700 dark:text-gray-400 sm:text-start">
-                Don&apos;t have an account? {""}
-                <Link
-                  to="/signup"
-                  className="text-brand-500 hover:text-brand-600 dark:text-brand-400"
-                >
-                  Sign Up
-                </Link>
-              </p>
-            </div>
           </div>
         </div>
       </div>
